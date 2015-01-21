000c23f8605c1ed60cbf93ad6e515ba99e32ac32
Jeremy Zilar <jeremyzilar@gmail.com>
2015-01-21 16:45:40 -0500

    Produce Price List

---
+++ t_produce_price_list.php
@@ -0,0 +1,42 @@
+<?php
+/* Template Name: Produce Price List */
+get_header();?>
+
+  <section id="t_<?php page_template_id(); ?>" class="main">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+
+          <table class="table">
+            <tr>
+              <th>Head</th>
+              <th>Head</th>
+              <th>Head</th>
+              <th>Head</th>
+              <th>Head</th>
+            </tr>
+            <tr>
+              <th>Head</th>
+              <td>Food</td>
+              <td>Food</td>
+              <td>Food</td>
+              <td>Food</td>
+            </tr>
+            <tr>
+              <th>Head</th>
+              <td>Food</td>
+              <td>Food</td>
+              <td>Food</td>
+              <td>Food</td>
+            </tr>
+          </table>
+
+          <div class="wbox" wbox-label="Block" wbox-type="block"></div>
+
+        </div>
+      </div>
+
+    </div>
+  </section>
+  
+<?php get_footer(); ?>
