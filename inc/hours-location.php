f479862e363e9a2a1434e511749b0576787d0b28
Jeremy Zilar <jeremyzilar@gmail.com>
2015-02-06 13:08:57 -0500

    Hours and location

---
+++ inc/hours-location.php
@@ -0,0 +1,10 @@
+<section id="hours-location" class="strip">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <h3 class="heading">Hours &amp; Location</h3>
+        <h4 class="heading">Sub-heading</h4>
+      </div>
+    </div>
+  </div>
+</section>
