013ebaad16d166893f8517d0c2960cc8175bd370
Jeremy Zilar <jeremyzilar@gmail.com>
2015-02-06 11:19:18 -0500

    abstract the head

--- inc/head.php
+++ inc/head.php
@@ -1,12 +1,10 @@
-<section id="hero" class="">
+<section id="head">
   <div class="container">
     <div class="row">
-      <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php echo THEME . '/img/psfc_logo_white.png'; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" class="img-responsive logo" /></a></h1>
-    </div>
-    <div class="row">
-    	<div class="col-xs-12">
-    		<h2>The Coop is redesigning its website,<br/> and you&#8217;re a part of it.</h2>
-    	</div>
+      <div class="col-xs-12">
+        <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php echo THEME . '/img/psfc_logo.png'; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" class="img-responsive logo" /></a></h1>
+        <h2>Good Food at Low Prices for Working Members through Cooperation since 1973</h2>
+      </div>
     </div>
   </div>
 </section>
--- t_homepage.php
+++ t_homepage.php
@@ -2,18 +2,9 @@
 /* Template Name: Homepage */
 get_header();?>
 
-  <section id="head">
-    <div class="container">
-      <div class="row">
-        <div class="col-xs-12">
-          <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php echo THEME . '/img/psfc_logo.png'; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" class="img-responsive logo" /></a></h1>
-          <h2>Good Food at Low Prices for Working Members through Cooperation since 1973</h2>
-        </div>
-      </div>
-    </div>
-  </section>
 
-
+  <?php include(INC . 'head.php'); ?>
+  
   <section id="t_<?php page_template_id(); ?>" class="main">
     <div class="container">
       
