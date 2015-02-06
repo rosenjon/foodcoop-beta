253d51547b79d1edf9b7681d13dc89da1a587801
Jeremy Zilar <jeremyzilar@gmail.com>
2015-02-06 12:14:42 -0500

    added bg photo to head

--- inc/head.php
+++ inc/head.php
@@ -1,8 +1,9 @@
 <section id="head">
+  <div id="hero-image"></div>
   <div class="container">
     <div class="row">
       <div class="col-xs-12">
-        <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php echo THEME . '/img/psfc_logo.png'; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" class="img-responsive logo" /></a></h1>
+        <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php echo THEME . '/img/psfc_logo_white.png'; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" class="img-responsive logo" /></a></h1>
         <h2>Good Food at Low Prices for Working Members through Cooperation since 1973</h2>
       </div>
     </div>
